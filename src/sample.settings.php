b8a98aafe67fc9101c5a0e8edebdd032973c74cc
Candle5000 <candle5000@icloud.com>
2019-05-08 23:57:05 +0900

    インデントをタブに統一

--- src/sample.settings.php
+++ src/sample.settings.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+	'settings' => [
+		'displayErrorDetails' => true, // set to false in production
+		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
 		// DB Access settings
 		'db' => [
@@ -12,16 +12,16 @@ return [
 			'pass' => 'samplepass'
 		],
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
+		// Renderer settings
+		'renderer' => [
+			'template_path' => __DIR__ . '/../templates/',
+		],
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'uruluk',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+		// Monolog settings
+		'logger' => [
+			'name' => 'uruluk',
+			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+			'level' => \Monolog\Logger::DEBUG,
+		],
+	],
 ];
