04d5f6bd124f38f22cdac2f499bf2ffcd2ff3871
Candle5000 <candle5000@icloud.com>
2019-05-12 18:35:41 +0900

    アクセスカウントテーブルを追加,	DDLの記述を物理名順に変更

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,5 +1,5 @@
 -- Project Name : Uruluk
--- Date/Time    : 2019/05/06 20:06:56
+-- Date/Time    : 2019/05/12 14:59:21
 -- Author       : Candle
 -- RDBMS Type   : MySQL
 -- Application  : A5:SQL Mk-2
@@ -10,31 +10,192 @@
   この機能は一時的に $$TableName のような一時テーブルを作成します。
 */
 
--- Urulukユーザ
+-- アクセスカウント
 --* RestoreFromTempTable
-create table uruluk_user (
-  user_id INT not null AUTO_INCREMENT comment 'ユーザID'
-  , login_id VARCHAR(64) not null comment 'ログインID'
-  , user_name VARCHAR(64) not null comment 'ユーザ名'
-  , mail_address VARCHAR(256) comment 'メールアドレス'
-  , login_password CHAR(64) comment 'パスワード'
-  , google_id VARCHAR(256) comment 'GoogleID'
-  , facebook_id VARCHAR(256) comment 'FacebookID'
-  , twitter_id VARCHAR(256) comment 'TwitterID'
-  , permission_level INT default 1 not null comment '権限レベル'
-  , last_login_datetime DATETIME comment '最終ログイン日時'
-  , created_datetime DATETIME comment '作成日時'
-  , constraint uruluk_user_PKC primary key (user_id)
-) comment 'Urulukユーザ' ;
+create table access_count (
+  page_id INT not null comment 'ページID'
+  , count_date DATE not null comment 'カウント日付'
+  , pv_count INT default 0 not null comment 'PVカウント'
+  , constraint access_count_PKC primary key (page_id,count_date)
+) comment 'アクセスカウント' ;
 
--- アイテムスキルログ
+-- 性能
 --* RestoreFromTempTable
-create table item_skill_log (
-  item_log_id INT not null comment 'ログID'
+create table attribute (
+  attribute_id INT not null AUTO_INCREMENT comment '性能ID'
+  , short_name VARCHAR(8) comment '略称'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , sort_key INT not null comment 'ソート順'
+  , constraint attribute_PKC primary key (attribute_id)
+) comment '性能' ;
+
+alter table attribute add unique attribute_IX1 (sort_key) ;
+
+-- ベースアイテム
+--* RestoreFromTempTable
+create table base_item (
+  base_item_id INT not null AUTO_INCREMENT comment 'ベースアイテムID'
+  , item_class_id INT not null comment 'アイテムクラスID'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , image_name VARCHAR(64) comment '画像名称'
+  , sort_key INT not null comment 'ソート順'
+  , constraint base_item_PKC primary key (base_item_id)
+) comment 'ベースアイテム' ;
+
+alter table base_item add unique base_item_IX1 (sort_key) ;
+
+-- キャラクタークラス性能
+--* RestoreFromTempTable
+create table character_attribute (
+  character_class_id INT not null comment 'キャラクタークラスID'
+  , attribute_id INT not null comment '性能ID'
+  , attribute_value FLOAT not null comment '性能値'
+  , constraint character_attribute_PKC primary key (character_class_id,attribute_id)
+) comment 'キャラクタークラス性能' ;
+
+-- キャラクタークラス
+--* RestoreFromTempTable
+create table character_class (
+  character_class_id INT not null AUTO_INCREMENT comment 'キャラクタークラスID'
+  , short_name VARCHAR(8) comment '略称'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , sort_key INT not null comment 'ソート順'
+  , constraint character_class_PKC primary key (character_class_id)
+) comment 'キャラクタークラス' ;
+
+alter table character_class add unique character_class_IX1 (sort_key) ;
+
+-- クリーチャー
+--* RestoreFromTempTable
+create table creature (
+  creature_id INT not null comment 'クリーチャーID'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , xp INT comment '経験値'
+  , note TEXT comment '説明'
+  , constraint creature_PKC primary key (creature_id)
+) comment 'クリーチャー' ;
+
+-- クリーチャー性能
+--* RestoreFromTempTable
+create table creature_attribute (
+  creature_id INT not null comment 'クリーチャーID'
+  , attribute_id INT not null comment '性能ID'
+  , attribute_value FLOAT comment '性能値'
+  , constraint creature_attribute_PKC primary key (creature_id,attribute_id)
+) comment 'クリーチャー性能' ;
+
+-- クリーチャードロップアイテム
+--* RestoreFromTempTable
+create table creature_drop_item (
+  creature_id INT not null comment 'クリーチャーID'
+  , item_id INT not null comment 'アイテムID'
+  , constraint creature_drop_item_PKC primary key (creature_id,item_id)
+) comment 'クリーチャードロップアイテム' ;
+
+-- クリーチャースペシャルアタック
+--* RestoreFromTempTable
+create table creature_special_attack (
+  creature_id INT not null comment 'クリーチャーID'
+  , special_attack_id INT not null comment 'スペシャルアタックID'
+  , constraint creature_special_attack_PKC primary key (creature_id,special_attack_id)
+) comment 'クリーチャースペシャルアタック' ;
+
+-- フロア
+--* RestoreFromTempTable
+create table floor (
+  floor_id INT not null comment 'フロアID'
+  , floor_group_id INT not null comment 'フロアグループID'
+  , short_name VARCHAR(16) comment '略称'
   , name_en VARCHAR(64) comment '名称(英語)'
   , name_ja VARCHAR(64) comment '名称(日本語)'
-  , constraint item_skill_log_PKC primary key (item_log_id)
-) comment 'アイテムスキルログ' ;
+  , sort_key INT not null comment 'ソート順'
+  , constraint floor_PKC primary key (floor_id)
+) comment 'フロア' ;
+
+alter table floor add unique floor_IX1 (sort_key) ;
+
+-- フロアクリーチャー
+--* RestoreFromTempTable
+create table floor_creature (
+  floor_id INT not null comment 'フロアID'
+  , creature_id INT not null comment 'クリーチャーID'
+  , constraint floor_creature_PKC primary key (floor_id,creature_id)
+) comment 'フロアクリーチャー' ;
+
+-- フロア移動先
+--* RestoreFromTempTable
+create table floor_destination (
+  floor_id INT not null comment '移動元フロアID'
+  , destination_floor_id INT not null comment '移動先フロアID'
+  , constraint floor_destination_PKC primary key (floor_id,destination_floor_id)
+) comment 'フロア移動先' ;
+
+-- フロアドロップアイテム
+--* RestoreFromTempTable
+create table floor_drop_item (
+  floor_id INT not null comment 'フロアID'
+  , item_id INT not null comment 'アイテムID'
+  , constraint floor_drop_item_PKC primary key (floor_id,item_id)
+) comment 'フロアドロップアイテム' ;
+
+-- フロアグループ
+--* RestoreFromTempTable
+create table floor_group (
+  floor_group_id INT not null AUTO_INCREMENT comment 'フロアグループID'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , sort_key INT not null comment 'ソート順'
+  , constraint floor_group_PKC primary key (floor_group_id)
+) comment 'フロアグループ' ;
+
+alter table floor_group add unique floor_group_IX1 (sort_key) ;
+
+-- アイテム
+--* RestoreFromTempTable
+create table item (
+  item_id INT not null AUTO_INCREMENT comment 'アイテムID'
+  , item_class_id INT not null comment 'アイテムクラスID'
+  , base_item_id INT comment 'ベースアイテムID'
+  , brand_id INT comment 'ブランドID'
+  , name_en VARCHAR(64) comment '名称(英語)'
+  , name_ja VARCHAR(64) comment '名称(日本語)'
+  , rarity ENUM('common', 'rare', 'artifact') not null comment 'レアリティ'
+  , comment_en VARCHAR(64) comment 'コメント(英語)'
+  , comment_ja VARCHAR(64) comment 'コメント(日本語)'
+  , note TEXT comment '説明'
+  , price INT comment '売却価格'
+  , imgae_name VARCHAR(64) comment '画像名称'
+  , sort_key INT not null comment 'ソート順'
+  , constraint item_PKC primary key (item_id)
+) comment 'アイテム' ;
+
+alter table item add unique item_IX1 (sort_key) ;
+
+-- アイテム性能
+--* RestoreFromTempTable
+create table item_attribute (
+  item_id INT not null comment 'アイテムID'
+  , attribute_id INT not null comment '性能ID'
+  , character_class_id INT not null comment 'キャラクタークラスID'
+  , attribute_value FLOAT not null comment '性能値'
+  , constraint item_attribute_PKC primary key (item_id,attribute_id,character_class_id)
+) comment 'アイテム性能' ;
+
+-- アイテム変動性能
+--* RestoreFromTempTable
+create table item_attribute_flactuable (
+  item_id INT not null comment 'アイテムID'
+  , attribute_id INT not null comment '性能ID'
+  , character_class_id INT not null comment 'キャラクタークラスID'
+  , attribute_value INT not null comment '性能値'
+  , based_on_xp_max INT default 0 not null comment 'EXPベース最大値'
+  , based_on_kills_max INT default 0 not null comment '討伐数ベース最大値'
+  , constraint item_attribute_flactuable_PKC primary key (item_id,attribute_id,character_class_id)
+) comment 'アイテム変動性能' ;
 
 -- アイテム変動性能ログ
 --* RestoreFromTempTable
@@ -60,6 +221,31 @@ create table item_attribute_log (
   , constraint item_attribute_log_PKC primary key (item_log_id,attribute_id,character_class_id)
 ) comment 'アイテム性能ログ' ;
 
+-- アイテムブランド
+--* RestoreFromTempTable
+create table item_brand (
+  brand_id INT not null AUTO_INCREMENT comment 'ブランドID'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , sort_key INT not null comment 'ソート順'
+  , constraint item_brand_PKC primary key (brand_id)
+) comment 'アイテムブランド' ;
+
+alter table item_brand add unique item_brand_IX1 (sort_key) ;
+
+-- アイテムクラス
+--* RestoreFromTempTable
+create table item_class (
+  item_class_id INT not null AUTO_INCREMENT comment 'アイテムクラスID'
+  , name_en VARCHAR(32) comment '名称(英語)'
+  , name_ja VARCHAR(32) comment '名称(日本語)'
+  , image_name VARCHAR(64) comment '画像名称'
+  , sort_key INT not null comment 'ソート順'
+  , constraint item_class_PKC primary key (item_class_id)
+) comment 'アイテムクラス' ;
+
+alter table item_class add unique item_class_IX1 (sort_key) ;
+
 -- アイテムログ
 --* RestoreFromTempTable
 create table item_log (
@@ -84,23 +270,23 @@ create table item_log (
   , constraint item_log_PKC primary key (item_log_id)
 ) comment 'アイテムログ' ;
 
--- アイテム性能
+-- アイテムスキル
 --* RestoreFromTempTable
-create table item_attribute (
-  item_id INT not null comment 'アイテムID'
-  , attribute_id INT not null comment '性能ID'
-  , character_class_id INT not null comment 'キャラクタークラスID'
-  , attribute_value FLOAT not null comment '性能値'
-  , constraint item_attribute_PKC primary key (item_id,attribute_id,character_class_id)
-) comment 'アイテム性能' ;
+create table item_skill (
+  item_id INT not null AUTO_INCREMENT comment 'アイテムID'
+  , name_en VARCHAR(64) comment '名称(英語)'
+  , name_ja VARCHAR(64) comment '名称(日本語)'
+  , constraint item_skill_PKC primary key (item_id)
+) comment 'アイテムスキル' ;
 
--- クリーチャースペシャルアタック
+-- アイテムスキルログ
 --* RestoreFromTempTable
-create table creature_special_attack (
-  creature_id INT not null comment 'クリーチャーID'
-  , special_attack_id INT not null comment 'スペシャルアタックID'
-  , constraint creature_special_attack_PKC primary key (creature_id,special_attack_id)
-) comment 'クリーチャースペシャルアタック' ;
+create table item_skill_log (
+  item_log_id INT not null comment 'ログID'
+  , name_en VARCHAR(64) comment '名称(英語)'
+  , name_ja VARCHAR(64) comment '名称(日本語)'
+  , constraint item_skill_log_PKC primary key (item_log_id)
+) comment 'アイテムスキルログ' ;
 
 -- スペシャルアタック
 --* RestoreFromTempTable
@@ -112,197 +298,20 @@ create table special_attack (
   , constraint special_attack_PKC primary key (special_attack_id)
 ) comment 'スペシャルアタック' ;
 
--- フロア移動先
+-- Urulukユーザ
 --* RestoreFromTempTable
-create table floor_destination (
-  floor_id INT not null comment '移動元フロアID'
-  , destination_floor_id INT not null comment '移動先フロアID'
-  , constraint floor_destination_PKC primary key (floor_id,destination_floor_id)
-) comment 'フロア移動先' ;
-
--- クリーチャー性能
---* RestoreFromTempTable
-create table creature_attribute (
-  creature_id INT not null comment 'クリーチャーID'
-  , attribute_id INT not null comment '性能ID'
-  , attribute_value FLOAT comment '性能値'
-  , constraint creature_attribute_PKC primary key (creature_id,attribute_id)
-) comment 'クリーチャー性能' ;
-
--- フロアクリーチャー
---* RestoreFromTempTable
-create table floor_creature (
-  floor_id INT not null comment 'フロアID'
-  , creature_id INT not null comment 'クリーチャーID'
-  , constraint floor_creature_PKC primary key (floor_id,creature_id)
-) comment 'フロアクリーチャー' ;
-
--- クリーチャードロップアイテム
---* RestoreFromTempTable
-create table creature_drop_item (
-  creature_id INT not null comment 'クリーチャーID'
-  , item_id INT not null comment 'アイテムID'
-  , constraint creature_drop_item_PKC primary key (creature_id,item_id)
-) comment 'クリーチャードロップアイテム' ;
-
--- クリーチャー
---* RestoreFromTempTable
-create table creature (
-  creature_id INT not null comment 'クリーチャーID'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , xp INT comment '経験値'
-  , note TEXT comment '説明'
-  , constraint creature_PKC primary key (creature_id)
-) comment 'クリーチャー' ;
-
--- フロアドロップアイテム
---* RestoreFromTempTable
-create table floor_drop_item (
-  floor_id INT not null comment 'フロアID'
-  , item_id INT not null comment 'アイテムID'
-  , constraint floor_drop_item_PKC primary key (floor_id,item_id)
-) comment 'フロアドロップアイテム' ;
-
--- フロア
---* RestoreFromTempTable
-create table floor (
-  floor_id INT not null comment 'フロアID'
-  , floor_group_id INT not null comment 'フロアグループID'
-  , short_name VARCHAR(16) comment '略称'
-  , name_en VARCHAR(64) comment '名称(英語)'
-  , name_ja VARCHAR(64) comment '名称(日本語)'
-  , sort_key INT not null comment 'ソート順'
-  , constraint floor_PKC primary key (floor_id)
-) comment 'フロア' ;
-
-alter table floor add unique floor_IX1 (sort_key) ;
-
--- ベースアイテム
---* RestoreFromTempTable
-create table base_item (
-  base_item_id INT not null AUTO_INCREMENT comment 'ベースアイテムID'
-  , item_class_id INT not null comment 'アイテムクラスID'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , image_name VARCHAR(64) comment '画像名称'
-  , sort_key INT not null comment 'ソート順'
-  , constraint base_item_PKC primary key (base_item_id)
-) comment 'ベースアイテム' ;
-
-alter table base_item add unique base_item_IX1 (sort_key) ;
-
--- アイテムブランド
---* RestoreFromTempTable
-create table item_brand (
-  brand_id INT not null AUTO_INCREMENT comment 'ブランドID'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , sort_key INT not null comment 'ソート順'
-  , constraint item_brand_PKC primary key (brand_id)
-) comment 'アイテムブランド' ;
-
-alter table item_brand add unique item_brand_IX1 (sort_key) ;
-
--- フロアグループ
---* RestoreFromTempTable
-create table floor_group (
-  floor_group_id INT not null AUTO_INCREMENT comment 'フロアグループID'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , sort_key INT not null comment 'ソート順'
-  , constraint floor_group_PKC primary key (floor_group_id)
-) comment 'フロアグループ' ;
-
-alter table floor_group add unique floor_group_IX1 (sort_key) ;
-
--- アイテムスキル
---* RestoreFromTempTable
-create table item_skill (
-  item_id INT not null AUTO_INCREMENT comment 'アイテムID'
-  , name_en VARCHAR(64) comment '名称(英語)'
-  , name_ja VARCHAR(64) comment '名称(日本語)'
-  , constraint item_skill_PKC primary key (item_id)
-) comment 'アイテムスキル' ;
-
--- キャラクタークラス性能
---* RestoreFromTempTable
-create table character_attribute (
-  character_class_id INT not null comment 'キャラクタークラスID'
-  , attribute_id INT not null comment '性能ID'
-  , attribute_value FLOAT not null comment '性能値'
-  , constraint character_attribute_PKC primary key (character_class_id,attribute_id)
-) comment 'キャラクタークラス性能' ;
-
--- アイテム変動性能
---* RestoreFromTempTable
-create table item_attribute_flactuable (
-  item_id INT not null comment 'アイテムID'
-  , attribute_id INT not null comment '性能ID'
-  , character_class_id INT not null comment 'キャラクタークラスID'
-  , attribute_value INT not null comment '性能値'
-  , based_on_xp_max INT default 0 not null comment 'EXPベース最大値'
-  , based_on_kills_max INT default 0 not null comment '討伐数ベース最大値'
-  , constraint item_attribute_flactuable_PKC primary key (item_id,attribute_id,character_class_id)
-) comment 'アイテム変動性能' ;
-
--- 性能
---* RestoreFromTempTable
-create table attribute (
-  attribute_id INT not null AUTO_INCREMENT comment '性能ID'
-  , short_name VARCHAR(8) comment '略称'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , sort_key INT not null comment 'ソート順'
-  , constraint attribute_PKC primary key (attribute_id)
-) comment '性能' ;
-
-alter table attribute add unique attribute_IX1 (sort_key) ;
-
--- アイテム
---* RestoreFromTempTable
-create table item (
-  item_id INT not null AUTO_INCREMENT comment 'アイテムID'
-  , item_class_id INT not null comment 'アイテムクラスID'
-  , base_item_id INT comment 'ベースアイテムID'
-  , brand_id INT comment 'ブランドID'
-  , name_en VARCHAR(64) comment '名称(英語)'
-  , name_ja VARCHAR(64) comment '名称(日本語)'
-  , rarity ENUM('common', 'rare', 'artifact') not null comment 'レアリティ'
-  , comment_en VARCHAR(64) comment 'コメント(英語)'
-  , comment_ja VARCHAR(64) comment 'コメント(日本語)'
-  , note TEXT comment '説明'
-  , price INT comment '売却価格'
-  , imgae_name VARCHAR(64) comment '画像名称'
-  , sort_key INT not null comment 'ソート順'
-  , constraint item_PKC primary key (item_id)
-) comment 'アイテム' ;
-
-alter table item add unique item_IX1 (sort_key) ;
-
--- アイテムクラス
---* RestoreFromTempTable
-create table item_class (
-  item_class_id INT not null AUTO_INCREMENT comment 'アイテムクラスID'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , image_name VARCHAR(64) comment '画像名称'
-  , sort_key INT not null comment 'ソート順'
-  , constraint item_class_PKC primary key (item_class_id)
-) comment 'アイテムクラス' ;
-
-alter table item_class add unique item_class_IX1 (sort_key) ;
-
--- キャラクタークラス
---* RestoreFromTempTable
-create table character_class (
-  character_class_id INT not null AUTO_INCREMENT comment 'キャラクタークラスID'
-  , short_name VARCHAR(8) comment '略称'
-  , name_en VARCHAR(32) comment '名称(英語)'
-  , name_ja VARCHAR(32) comment '名称(日本語)'
-  , sort_key INT not null comment 'ソート順'
-  , constraint character_class_PKC primary key (character_class_id)
-) comment 'キャラクタークラス' ;
-
-alter table character_class add unique character_class_IX1 (sort_key) ;
+create table uruluk_user (
+  user_id INT not null AUTO_INCREMENT comment 'ユーザID'
+  , login_id VARCHAR(64) not null comment 'ログインID'
+  , user_name VARCHAR(64) not null comment 'ユーザ名'
+  , mail_address VARCHAR(256) comment 'メールアドレス'
+  , login_password CHAR(64) comment 'パスワード'
+  , google_id VARCHAR(256) comment 'GoogleID'
+  , facebook_id VARCHAR(256) comment 'FacebookID'
+  , twitter_id VARCHAR(256) comment 'TwitterID'
+  , permission_level INT default 1 not null comment '権限レベル'
+  , last_login_datetime DATETIME comment '最終ログイン日時'
+  , created_datetime DATETIME comment '作成日時'
+  , constraint uruluk_user_PKC primary key (user_id)
+) comment 'Urulukユーザ' ;
 
