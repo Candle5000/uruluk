bf1b90d6403f34d0f80e143d4d2c6c6b51c098b6
Candle5000 <candle5000@icloud.com>
2019-07-15 20:45:58 +0900

    ブーストアップの初期値を変更

--- src/classes/controller/SimulatorController.php
+++ src/classes/controller/SimulatorController.php
@@ -27,7 +27,7 @@ class SimulatorController extends Controller {
 		$xp = filter_var($xp, FILTER_VALIDATE_INT, ['options' => ['default' => self::XP_DEFAULT, 'min_range' => 0]]);
 		$kills = array_key_exists('kills', $getParam) ? $getParam['kills'] : self::KILLS_DEFAULT;
 		$kills = filter_var($kills, FILTER_VALIDATE_INT, ['options' => ['default' => self::KILLS_DEFAULT, 'min_range' => 0]]);
-		$boostups = array_key_exists('b', $getParam) && is_array($getParam['b']) && count($getParam['b']) == 5 ? $getParam['b'] : [0, 0, 0, 0, 0];
+		$boostups = array_key_exists('b', $getParam) && is_array($getParam['b']) && count($getParam['b']) == 5 ? $getParam['b'] : [2, 4, 1, 1, 1];
 		foreach ($boostups as $index => $boostup) {
 			$boostups[self::BOOSTUPS_NAME[$index]] = filter_var($boostup, FILTER_VALIDATE_INT, ['options' => ['default' => 0, 'min_range' => 0]]);
 		}
